6f24e04bd0aecca9152c5fe627a7f47ee3f7d9e3
Sivakumar U <itsmesivakumar21@gmail.com>
2025-06-20 15:53:25 -0400

    [Siva]Added: Complete backend application code

---
+++ src/main/java/com/siva/ems/dto/EmployeeDto.java
@@ -0,0 +1,23 @@
+package com.siva.ems.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+/*
+ * We created EmployeeDto class to transfer data from client to server.
+ */
+public class EmployeeDto {
+	
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String email;
+
+}
---
+++ src/main/java/com/siva/ems/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.siva.ems.service;
+
+import java.util.List;
+
+import com.siva.ems.dto.EmployeeDto;
+
+public interface EmployeeService {
+
+	EmployeeDto createEmployee(EmployeeDto employeeDto);
+	
+	EmployeeDto getEmployeeById(Long employeeId);
+	
+	List<EmployeeDto> getAllEmployees();
+	
+	EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
+	
+	void deleteEmployee(Long employeeId);
+
+}
